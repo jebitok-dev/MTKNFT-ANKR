81a4580305267a0edc214071033c0851372abf75
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-06-01 23:56:12 +0300

    Add the Contract

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+const dotenv = require("dotenv");
+dotenv.config({path: __dirname + '/.env'});
+const { RPC_URL, PRIVATE_KEY } = process.env;
+require("@nomiclabs/hardhat-ethers")
+
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: RPC_URL || "",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+    },
+  },
+};
